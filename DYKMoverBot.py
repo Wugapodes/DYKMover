f3737bf6083409cd6c24c65d110d44809670b674
Wugapodes <wugapodes@aol.com>
2017-02-12 16:58:29 -0500

    switch argument order, v0.8.0

--- DYKMoverBot.py
+++ DYKMoverBot.py
@@ -104,7 +104,7 @@ def checkPage(title):
     else:
         pass
             
-def computeNomStatus(link,status=0,pageName):
+def computeNomStatus(link,pageName,status=0):
     global nclosed
     global aclosed
     global pageCnt
