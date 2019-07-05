72939b930574d6fae966ba6b60bdc5108732249b
Wugapodes <wugapodes@aol.com>
2019-07-05 00:10:57 -0700

    Refactor and fix notification bug.

--- DYKNotification.py
+++ DYKNotification.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-__version__ = '0.3.0-dev'
+__version__ = '0.4.0-dev'
 
 import re
 import os
@@ -20,9 +20,7 @@ CONFIGURATION VARIABLES
 The following variables toggle the way the bot interacts with pages and their
     setting should reflect consensus.
 ATTRIBUTE   VALUES  DESCRIPTION
-live        -1      Debug mode. Only reads from test pages. Does not write to
-                      Wikipedia at all, just computes and exits.
-             0      Testing mode. Reads from the live DYKN pages. Writes to
+live         0      Testing mode. Reads from the live DYKN pages. Writes to
                       test pages only.
              1      Live mode. The bot is live and edits outside Userspace.
                       This mode should not be used without approval from the
@@ -41,72 +39,91 @@ def editLink(nom):
     url = pref + title + action
     return(url)
     
-
-site = pywikibot.Site('en', 'wikipedia')
-if live <= -1:
-    read  = 'User:Wugapodes/DYKTest'
-    write = 'User:Wugapodes/DYKTest'
-elif live == 0:
-    read  = 'Template talk:Did you know'
-    write = 'User:Wugapodes/DYKTest'
-elif live == 1:
-    read  = 'Template talk:Did you know'
-    write = 'Template talk:Did you know'
-
-nomPage = pywikibot.Page(site,read)
-approvedPage = pywikibot.Page(site,read+'/Approved')
-
-n_text = nomPage.text
-a_text = approvedPage.text
-all_text = n_text+"\n"+a_text
-all_lines = all_text.split("\n")
-noms = []
-for line in all_lines:
-    if "==" in line:
-        continue
-    elif "<!--" in line:
-        continue
-    elif "{{" in line:
-        noms.append(line)
-        
-# For Testing Purposes
-shuffle(noms)
-        
-for nom in noms:
-    if _n == _debug_n:
-        break
-    try:
-        pageTitle = re.search(r"{{.*?nominations\/(.*?)}}",nom).group(1)
-    except:
-        print(nom)
-        continue
-    article = pywikibot.Page(site,pageTitle)
-    if article.pageid == 0:
-        print("Article does not exist")
-        continue
-    elif article.isRedirectPage():
-        redirect = article.getRedirectTarget()
-        article = pywikibot.Page(site,redirect)
-    talk = article.toggleTalkPage()
-    talk_text = talk.text
-    if nom in talk_text:
-        print("Already notified")
-        continue
+def already_notified(text):
+    dyk_regex = r'\{\{.*?Did(?: |_)you(?: |_)know(?: |_)nominations\/.*?\}\}'
+    matches = re.search(dyk_regex,text)
+    if matches:
+        return(True)
     else:
-        if "{{nobots}}" in talk_text:
-            continue
-        elif "{{bots" in talk_text:
-            m = re.search(r"{{bots\|.*?deny=.*?(WugBot).*?(?:\|allow|}})",talk_text)
-            try:
-                if m.group(1) == "WugBot":
-                    continue
-            except:
-                pass
-        talk_text = talk_text \
-            + "\n\n" + nom
-    if live != 1:
-        talk = pywikibot.Page(site,"User:WugBot/DYKNoteTest")
-    talk.text = talk_text
-    talk.save("This article has been nominated at [[WP:DYKN]] to be featured on the main page. WugBot v"+__version__)
-    _n += 1
+        return(False)
+
+def is_nom(line):
+    status = already_notified(line)
+    return(status)
         
+def excluded(text):
+    if "{{nobots}}" in text:
+        return(True)
+    elif "{{bots" in text:
+        m = re.search(r"{{bots\|.*?deny=.*?(WugBot).*?(?:\|allow|}})",text)
+        if m:
+            return(True)
+        else:
+            return(False)
+    else:
+        return(False)
+        
+def pages(live,test_page='User:WugBot/DYKNoteTest'):
+    if live == 1:
+        read  = 'Template talk:Did you know'
+        write = None
+    else:
+        read = test_page
+        write = 'User:Wugapodes/DYKTest'
+    io = (read,write)
+    return(io)
+    
+def main():
+    global live
+    global _n
+    global _debug_n
+    site = pywikibot.Site('en', 'wikipedia')
+    read,write = pages(live)
+    nomPage = pywikibot.Page(site,read)
+    approvedPage = pywikibot.Page(site,read+'/Approved')
+    
+    n_text = nomPage.text
+    a_text = approvedPage.text
+    all_text = n_text+"\n"+a_text
+    all_lines = all_text.split("\n")
+    noms = []
+    for line in all_lines:
+        if is_nom(line)
+            noms.append(line)
+            
+    # For Testing Purposes
+    if live != 1:
+        shuffle(noms)
+            
+    for nom in noms:
+        if _n == _debug_n:
+            break
+        try:
+            pageTitle = re.search(r"{{.*?nominations\/(.*?)}}",nom).group(1)
+        except:
+            print(nom)
+            continue
+        article = pywikibot.Page(site,pageTitle)
+        if article.pageid == 0:
+            print("Article does not exist")
+            continue
+        elif article.isRedirectPage():
+            redirect = article.getRedirectTarget()
+            article = pywikibot.Page(site,redirect)
+        talk = article.toggleTalkPage()
+        talk_text = talk.text
+        if already_notified(talk_text):
+            print("Already notified")
+            continue
+        if excluded(talk_text):
+            continue
+        talk_text = talk_text + "\n\n" + nom
+        if live != 1:
+            talk = pywikibot.Page(site,write)
+        talk.text = talk_text
+        talk.save("This article has been nominated at [[WP:DYKN]] " \
+                    + "to be featured on the main page. WugBot v"+__version__)
+        _n += 1
+        
+if __name__ == "__main__":
+    main()
